97b6e00feae97b554d6fd5463db734bf76e8d51c
antsimch <anthonysimch@gmail.com>
2023-05-24 13:56:30 +0800

    initial commit

---
+++ src/main/java/sg/edu/nus/iss/ssf_workshop2revision/SsfWorkshop2revisionApplication.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_workshop2revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsfWorkshop2revisionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsfWorkshop2revisionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sg/edu/nus/iss/ssf_workshop2revision/SsfWorkshop2revisionApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.ssf_workshop2revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsfWorkshop2revisionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
